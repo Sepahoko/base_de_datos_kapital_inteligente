f51e55a88aa1820a933089704c0967b34bbae22f
amancio <cristianedjang@gmail.com>
2024-04-24 17:17:02 +0200

    segundo commit

--- bin/base_de_datos_kapital_inteligente.dart
+++
@@ -1,5 +0,0 @@
-import 'package:base_de_datos_kapital_inteligente/base_de_datos_kapital_inteligente.dart' as base_de_datos_kapital_inteligente;
-
-void main(List<String> arguments) {
-  print('Hello world: ${base_de_datos_kapital_inteligente.calculate()}!');
-}
---
+++ bin/usuario.dart
@@ -0,0 +1,20 @@
+import 'package:mysql1/mysql1.dart';
+
+abstract class Usuario {
+  //Propiedades
+  int? id;
+  String? nombre;
+  String? fechaNacimiento;
+  String? genero;
+  String? direccion;
+  String? telefono;
+  String? correoElectronico;
+
+  //Constructor
+  Usuario(this.id, this.nombre, this.fechaNacimiento, this.genero, this.direccion, this.telefono, this.correoElectronico);
+  fromMap(ResultRow row);
+
+  //Método para mostrar la información de la persona 
+  mostrarInformacion();
+
+} 
